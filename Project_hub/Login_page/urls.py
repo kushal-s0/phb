6305fcc1fa8f6254d46c3edfe9349501a1aed5cd
Sagar-Shetty0804 <sagarshetty.lm10judo@gmail.com>
2024-09-06 23:56:06 +0530

    adding login and register pages

---
+++ Project_hub/Login_page/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from Login_page import views
+
+urlpatterns = [
+    path("",views.studentPage,name="Sudent page"),
+    path('registerStudent/',views.registerStudent,name="register Student"),
+    
+]
--- Project_hub/Login_page/views.py
+++ Project_hub/Login_page/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def studentPage(request):
+    return render(request,'loginPage.html')
+
+def registerStudent(request):
+    return render(request,'registerStudent.html')
