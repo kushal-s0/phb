944890b6ff05e9d28a6dd7c710d86eb2c3ff5797
Sagar-Shetty0804 <sagarshetty.lm10judo@gmail.com>
2024-09-07 17:25:18 +0530

    update relational schema

--- Project_hub/Login_page/models.py
+++ Project_hub/Login_page/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Register(models.Model):
+    name = models.CharField(50)
+    username=models.CharField(50)
+    email=models.EmailField(max_length=50)
+    password=models.CharField(50)
+    confirmPassword=models.CharField(50)
