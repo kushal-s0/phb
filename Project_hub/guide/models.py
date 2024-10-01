792735e842823c7a422ef9bc296d870676a9d80d
Sagar-Shetty0804 <sagarshetty.lm10judo@gmail.com>
2024-10-01 23:18:30 +0530

    Gudie Database added

--- Project_hub/guide/models.py
+++ Project_hub/guide/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+    
+class guide_groups(models.Model):
+    groupCode = models.CharField(max_length=60,primary_key=True)
+    guide_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    projectName = models.CharField(max_length=60)
+
+    def __str__(self) -> str:
+        return self.guide_name.first_name
+    
+    class Meta:
+        verbose_name_plural = "Guide_group"
--- Project_hub/guide/views.py
+++ Project_hub/guide/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from Login_page.models import RegisterStudent as reg
-
+from django.contrib.auth.models import User
+from guide.models import guide_groups
 # Create your views here.
+
 def start(request):
 
     return render(request,'guide.html')
@@ -10,6 +12,9 @@ def groups(request):
     return render(request,'groups\\groups.html')
 
 def addGroup(request):
+    currentUser = request.user
+    
+    
     data = reg.objects.values('groupCode','projectName').distinct()
 
     data_dict = {'data':data}
@@ -17,6 +22,14 @@ def addGroup(request):
     if request.method == 'POST':
         
         groupCode = request.POST.getlist('code')
-        print(groupCode)
+        
+        
+        for gc in groupCode:
+            text = gc.split(',')
+            print(text)
+            
+            register2 = guide_groups(groupCode = text[0],guide_name = currentUser,projectName = text[1])
+            
+            register2.save()
 
     return render(request,'addGroup\\addGroup.html',data_dict)
