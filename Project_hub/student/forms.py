3693e31127108608630344fe025dc49307a62f5c
Kushal D Soni <kushal.soni@somaiya.edu>
2024-10-08 10:10:54 +0530

    Update forms.py

--- Project_hub/student/forms.py
+++ Project_hub/student/forms.py
@@ -1,17 +1,22 @@
 from django import forms
-from .models import CodeFile, DatabaseFile,DocumentFile,AdditionalFile
-
-class FileEditForm(forms.ModelForm):
-    class Meta:
-        model = CodeFile
-        fields = ['name', 'content','file','file_type','group_code']
-        widgets = {
-            'content': forms.Textarea(attrs={'rows': 20, 'cols': 100}),
-        }
-
-
+from .models import CodeFile, DatabaseFile, DocumentFile, AdditionalFile
 
 class CodeFileForm(forms.ModelForm):
     class Meta:
         model = CodeFile
-        fields = ['group_code', 'file', 'file_type', 'name', 'content'] 
+        fields = ['group_code', 'file']
+
+class DatabaseFileForm(forms.ModelForm):
+    class Meta:
+        model = DatabaseFile
+        fields = ['group_code', 'file']
+
+class DocumentFileForm(forms.ModelForm):
+    class Meta:
+        model = DocumentFile
+        fields = ['group_code', 'file']
+
+class AdditionalFileForm(forms.ModelForm):
+    class Meta:
+        model = AdditionalFile
+        fields = ['group_code', 'file']
