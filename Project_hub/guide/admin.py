9bb312c1c8647008731b9b30f8cca2d2918638f0
Kushal D Soni <kushal.soni@somaiya.edu>
2024-10-09 23:23:16 +0530

    Delete Project_hub directory

--- Project_hub/guide/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from guide.models import guide_groups
-
-# Register your models here.
-
-
-admin.site.register(guide_groups)
--- Project_hub/uploads/additional/script.js
+++
@@ -1,5 +0,0 @@
-// media/script.js
-console.log('This is an example JavaScript file.');
-function greet() {
-    alert('Hello, World!');
-}
